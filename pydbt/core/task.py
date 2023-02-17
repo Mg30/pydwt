3f6fb8a54a257ce3f43e563e6c7701008402cbf8
gonza <matgonzalez@hotmail.fr>
2023-02-17 16:27:45 +0100

    refact: create abstract base task

--- pydbt/core/task.py
+++ pydbt/core/task.py
@@ -1,19 +1,21 @@
-from dataclasses import dataclass, field
-from typing import Callable, List
-from pydbt.core.workflow import Workflow
-from pydbt.core.schedule import ScheduleInterface, Daily
 import functools
 import logging
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 from datetime import datetime, timedelta
+from typing import Callable, Dict, List
+
+from dependency_injector.wiring import Provide
+
 from pydbt.core.containers import Container
-from dependency_injector.wiring import  Provide
-from pydbt.core.containers import Container
-from typing import Dict
+from pydbt.core.schedule import Daily, ScheduleInterface
+from pydbt.core.workflow import Workflow
+
 
 @dataclass
-class Task:
+class BaseTask(ABC):
     """
-    Class representing a task in a DAG.
+    Abstract base class for representing a task in a DAG.
 
     :param depends_on: List of other tasks that this task depends on
     :param runs_on: Schedule for running this task. Default is `Daily()`
@@ -29,9 +31,9 @@ class Task:
     name: str = field(init=False)
     _next_run: datetime = field(init=False, default=None)
     _task: Callable = field(init=False, default=None)
-    workflow : Workflow = Provide[Container.workflow_factory]
-    config : Dict = Provide[Container.config]
-    sources : Dict = Provide[Container.datasources]
+    workflow: Workflow = Provide[Container.workflow_factory]
+    config: Dict = Provide[Container.config]
+    sources: Dict = Provide[Container.datasources]
 
     @property
     def depends_on_name(self):
@@ -66,17 +68,17 @@ class Task:
         self._task = func
         self.name = f"{func.__module__}.{func.__name__}"
 
-        logging.info(f"registring task {self.name}")
+        logging.info(f"registering task {self.name}")
         self.workflow.tasks.append(self)
 
         @functools.wraps(func)
-        def wrapper(*args,**kwargs):
-            return func(*args,**kwargs)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
 
         return wrapper
 
     def __eq__(self, other):
-        if isinstance(other, Task):
+        if isinstance(other, BaseTask):
             return (
                 set(self.depends_on_name) == set(other.depends_on_name)
                 and self.runs_on_name == other.runs_on_name
@@ -85,6 +87,34 @@ class Task:
                 and self.name == other.name
             )
         return False
+
+    @abstractmethod
+    def run(self):
+        """
+        Run this task.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _run_task_with_retry(self):
+        raise NotImplementedError
+
+
+@dataclass
+class Task(BaseTask):
+    """
+    Class representing a task in a DAG.
+
+    :param depends_on: List of other tasks that this task depends on
+    :param runs_on: Schedule for running this task. Default is `Daily()`
+    :param retry: Number of times to retry this task in case of failure
+    :param ttl_minutes: Time-to-live in minutes. If a positive value is provided, the task will only run
+                        if the time elapsed since the last run is greater than or equal to this value.
+    """
+
+    workflow: Workflow = Provide[Container.workflow_factory]
+    config: Dict = Provide[Container.config]
+    sources: Dict = Provide[Container.datasources]
 
     def run(self):
         """
@@ -114,7 +144,7 @@ class Task:
             try:
                 task_config = self.config["tasks"][self.name]
                 self._count_call += 1
-                self._task(task_config,self.sources)
+                self._task(task_config, self.sources)
                 break
             except Exception as e:
                 if n == self.retry:
@@ -127,7 +157,7 @@ class Task:
 
 
 @dataclass
-class AsyncTask:
+class AsyncTask(BaseTask):
     """
     Class representing a task in a DAG.
 
@@ -138,65 +168,9 @@ class AsyncTask:
                         if the time elapsed since the last run is greater than or equal to this value.
     """
 
-    depends_on: List[Callable] = field(default_factory=list)
-    runs_on: ScheduleInterface = field(default_factory=Daily)
-    retry: int = 0
-    ttl_minutes: int = 0
-    name: str = field(init=False)
-    _next_run: datetime = field(init=False, default=None)
-    _task: Callable = field(init=False, default=None)
-
-    @property
-    def depends_on_name(self):
-        return (
-            [f"{func.__module__}.{func.__name__}" for func in self.depends_on]
-            if self.depends_on
-            else []
-        )
-
-    @property
-    def runs_on_name(self):
-        return str(type(self.runs_on).__name__)
-
-    @property
-    def is_ttl_elapsed(self):
-        return self._next_run < datetime.now()
-
-    def set_next_run(self):
-        """
-        Set the time for the next run of this task.
-        """
-        now = datetime.now()
-        self._next_run = now + timedelta(minutes=self.ttl_minutes)
-        logging.info(
-            f"task {self.name}: found ttl definition next run: { self._next_run.strftime('%Y%m%d_%H:%M:%S')}"
-        )
-
-    def __call__(self, func: Callable):
-        """
-        Decorator for registering a task in the DAG.
-        """
-        self._task = func
-        self.name = f"{func.__module__}.{func.__name__}"
-        logging.info(f"registring task {self.name}")
-        Workflow.tasks.append(self)
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    def __eq__(self, other):
-        if isinstance(other, Task):
-            return (
-                set(self.depends_on_name) == set(other.depends_on_name)
-                and self.runs_on_name == other.runs_on_name
-                and self.retry == other.retry
-                and self.ttl_minutes == other.ttl_minutes
-                and self.name == other.name
-            )
-        return False
+    workflow: Workflow = Provide[Container.workflow_factory]
+    config: Dict = Provide[Container.config]
+    sources: Dict = Provide[Container.datasources]
 
     async def run(self):
         """
