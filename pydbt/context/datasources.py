46d46ce70c18fafb332cd0073f70d005b051ebb8
gonza <matgonzalez@hotmail.fr>
2023-02-17 17:31:58 +0100

    refact: datasources

--- pydbt/context/datasources.py
+++ pydbt/context/datasources.py
@@ -1,13 +1,34 @@
 from dataclasses import dataclass
 from pydbt.sql.session import Session
-from typing import Any
+from typing import Any, Dict
+
 
 @dataclass
 class Datasources(object):
-    referentiel: dict
+    """Class for accessing tables in a SQL database.
+
+    Attributes:
+        referentiel (dict): A dictionary that contains metadata about the database.
+        engine (Any): A SQLAlchemy engine object that connects to the database.
+    """
+
+    referentiel: Dict[str, dict]
     engine: Any
 
     def get_source(self, name: str):
+        """Returns a SQLAlchemy Table object for a given data source.
+
+        Args:
+            name (str): The name of the data source to retrieve.
+
+        Returns:
+            DataFrame: A DataFrame object representing the data source.
+        """
+        # Get the configuration for the data source
         config = self.referentiel[name]
+
+        # Create a Session object for the schema that contains the table
         session = Session(engine=self.engine, schema=config["schema"])
+
+        # Return a Table object for the table in the data source
         return session.table(config["table"])
