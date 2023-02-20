aefa010f35a295733a79c10cd9bc69a51225164d
gonza <matgonzalez@hotmail.fr>
2023-02-20 10:09:56 +0100

    refact: test suit for dag

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -11,49 +11,43 @@ container.database_client.override(unittest.mock.Mock())
 container.wire(modules=["pydbt.core.task"])
 
 
-@pytest.fixture
-def fake_task_one():
-    def inner_func():
-        pass
 
-    return inner_func
 
 
 @pytest.fixture
-def fake_task_two():
-    def inner_func_bis():
+def dag():
+
+    def fake_task_one():
+        pass
+    def fake_task_two():
         pass
 
-    return inner_func_bis
+    task1 = Task(retry=2)
+    task1(fake_task_one)
 
+    task2 = Task(depends_on=[fake_task_one])
+    task2(fake_task_two)
+    dag = Dag(tasks=[task1, task2])
+    dag.build_dag()
+    return dag
 
-def test_build_dag(fake_task_one, fake_task_two):
+def test_build_dag_nodes(dag):
     """Test that the dag is built correctly."""
-    task1 = Task(retry=2)
-    task1(fake_task_one)
-
-    task2 = Task(depends_on=[fake_task_one])
-    task2(fake_task_two)
-
-    dag = Dag(tasks=[task1, task2])
-    dag.build_dag()
-
     assert list(dag.graph.nodes()) == [0, 1, "s"]
+
+
+def test_build_dag_edges(dag):
+    """Test that the dag is built correctly."""
     assert list(dag.graph.edges()) == [(0, 1), ("s", 0)]
-    assert dag.graph.nodes[0]["name"] == "tests.test_dags.inner_func"
-    assert dag.graph.nodes[1]["name"] == "tests.test_dags.inner_func_bis"
 
 
-def test_build_level(fake_task_one, fake_task_two):
+def test_build_dag_node_name(dag):
+    """Test that the dag is built correctly."""
+    assert dag.graph.nodes[0]["name"] == "tests.test_dags.fake_task_one"
+
+
+
+def test_build_level(dag):
     """Test that the levels are assigned correctly."""
-    task1 = Task(retry=2)
-    task1(fake_task_one)
-
-    task2 = Task(depends_on=[fake_task_one])
-    task2(fake_task_two)
-
-    dag = Dag(tasks=[task1, task2])
-    dag.build_dag()
     dag.build_level()
-
     assert dag.levels == {0: ["s"], 1: [0], 2: [1]}
